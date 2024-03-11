a576b9caa50b9347a9220e0be5c902655bde1d76
manolli kassabian <manollikassabian3@gmail.com>
2024-03-11 19:55:55 +0200

    second commit update taks-details styles

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -6,10 +6,9 @@ import { useEffect, useState } from "react";
 const TodoList: React.FC<{todos:any[]}> = ({todos}) => {
   const [data, setData] = useState<any[]>(todos);
 
-  // Update state when todos prop changes
   useEffect(() => {
     setData(todos);
-  }, [todos]); // Run effect only when todos prop changes
+  }, [todos]); 
   return (
     <>
     <div className="p-2  text-gray-500 font-bold cursor-pointer">Click on the task to show the task details</div>
