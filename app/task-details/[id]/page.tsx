85975eb0e499c0ec2aed31d2a17ca3a130eb1bfa
manolli kassabian <manollikassabian3@gmail.com>
2024-03-11 11:05:53 +0200

    second commit update taks-details styles

--- app/task-details/[id]/page.tsx
+++ app/task-details/[id]/page.tsx
@@ -3,13 +3,18 @@ import axios from 'axios';
 const Page = async ({params}:any) => {  
 
     const responseData = await getData();
-    const title = responseData.data.result.related[params.id]?.title;
-    const imageUrl = responseData.data.result?.image;
+    const title = responseData.data.result.related[params.id++]?.title;
+    const imageUrl = responseData.data.result.related[params.id++]?.image;
   return (
-    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl lg:max-w-4xl">
-      <p className="text-gray-800 font-semibold text-lg px-4 py-2">{title}</p>
-      <img className="w-full" src={imageUrl} alt="Image" />
-    </div>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+  <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl lg:max-w-4xl">
+    <p className="text-gray-800 font-semibold text-lg px-4 py-2"> Title: {title}</p>
+    <img className="w-full" src={imageUrl} alt="Image" />
+  </div>
+
+  </div>
+
+
   );
 };
 
